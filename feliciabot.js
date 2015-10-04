dfa7ddd0e68db93e78aa97d087c97625fe13ddd6
Bobby Smith <bobbysmith24@gmail.com>
2015-10-04 14:30:08 -0500

    webhook maybe idk

--- feliciabot.js
+++ feliciabot.js
@@ -1,9 +1,10 @@
 module.exports = function (req, res, next) {
   var username = req.body.user_name;
-  var botPayload = 'http://giphy.com/gifs/friday-movie-craig-bye-felisha-l0O9xk5sLcmWmOkaQ';
+  var text = req.body.text;
+  var botPayload = text + ' ' + 'http://giphy.com/gifs/friday-movie-craig-bye-felisha-l0O9xk5sLcmWmOkaQ';
 
   if (username !== 'slackbot') {
-    return res.status(200).json(botPayload);
+    return res.status(200).json('https://hooks.slack.com/services/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', botPayload);
   } else {
     return res.status(200).end();
   }
