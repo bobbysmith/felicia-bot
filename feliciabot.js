4a2cdcdf71d1f3b9162db9a58e7804e4c952b9f1
Bobby Smith <bobbysmith24@gmail.com>
2015-10-04 14:39:10 -0500

    json for payload object

--- feliciabot.js
+++ feliciabot.js
@@ -1,6 +1,6 @@
 module.exports = function (req, res, next) {
   var username = req.body.user_name;
-  var botPayload = 'http://giphy.com/gifs/friday-movie-craig-bye-felisha-l0O9xk5sLcmWmOkaQ';
+  var botPayload = { text: 'http://giphy.com/gifs/friday-movie-craig-bye-felisha-l0O9xk5sLcmWmOkaQ'};
 
   if (username !== 'slackbot') {
     return res.status(200).json(botPayload);
