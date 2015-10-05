2aa6851a709fe99cf2acf964d1ed2a0d4e7dfc91
Bobby Smith <bobbysmith24@gmail.com>
2015-10-05 11:27:29 -0500

    slackbot response no worky

--- feliciabot.js
+++ feliciabot.js
@@ -1,9 +1,9 @@
 module.exports = function (req, res, next) {
   var username = req.body.user_name;
-  // var message = req.body.text;
+  var message = req.body.text;
   var botPayload = { text: 'https://media.giphy.com/media/SZioIIBxB7QRy/giphy.gif'};
 
-  if (username == 'slackbot') {
+  if (username !== 'slackbot') {
     return res.status(200).json(botPayload);
   } else {
     return res.status(200).end();
